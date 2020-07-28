17b630f689ccd13c02b256120d9cba83989dcff9
Adam Friedman <adam@blok.party>
2020-07-27 17:34:10 -0700

    Added variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variables
+const canvas = document.querySelector('canvas');
+const generateButton = document.querySelector('.generate-tree-button');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+let curve = 10;
+let curve2 = 0;
